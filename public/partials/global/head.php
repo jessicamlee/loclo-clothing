4666696ac25f12046d832bf319f9f235a3bb5b1c
jessicamlee <jessicamlee25@gmail.com>
2022-10-13 20:08:32 -0700

    Compiled all variables into single file
    Compiled all variables into single file

--- public/partials/global/head.php
+++ public/partials/global/head.php
@@ -8,11 +8,25 @@
     <title><?php echo $title_text; ?></title>
     <link rel="icon" type="image/icon" href="images/loclo_favicon.svg">
     <meta name="author" content="Jessica Lee &vert; https://jessicamlee.dev">
-    <meta name="description" content="LOCLO is a local small business brand&comma; focusing on providing quality goods while making a minimal impact on the environment&period;">
+    <meta name="description" content="LOCLO Clothing is a local small business brand, focusing on providing quality goods while making a minimal impact on the environment.">
     <meta name="keywords" content="local clothes vancouver, local clothes canada, loclo clothing, minimalist quality clothing canada">
+    <meta property="image" content="images/loclo_springlocal2022_cover.jpg">
+    <!-- OG Content -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://locloclothing.com">
+    <meta property="og:title" content="Spring Local 2022 | LOCLO Clothing">
+    <meta property="og:description" content="LOCLO clothing is a local small business brand, focusing on providing quality goods while making a minimal impact on the environment.">
+    <meta property="og:image" content="images/loclo_springlocal2022_cover.jpg">
+    <!-- Twitter Content -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://locloclothing.com">
+    <meta property="twitter:title" content="Spring Local 2022 | LOCLO Clothing">
+    <meta property="twitter:description" content="LOCLO clothing is a local small business brand, focusing on providing quality goods while making a minimal impact on the environment.">
+    <meta property="twitter:image" content="images/loclo_springlocal2022_cover.jpg">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="styles/reset.css">
     <link rel="stylesheet" href="../node_modules/aos/dist/aos.css">
     <link rel="stylesheet" href="styles/css/main.css">
